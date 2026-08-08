755dcb21aa7560e99818a43b345cedbaee788e52
arutyunov.eth <114887931+arutyunoveth@users.noreply.github.com>
2026-08-08 07:11:08 +0300

    P1.05: export gate reference types

--- reference/python/arvectum_os_ref/__init__.py
+++ reference/python/arvectum_os_ref/__init__.py
@@ -10,6 +10,15 @@ from .execution import (
     GovernedVersionPin,
     start_p1_04_execution,
 )
+from .gates import (
+    GateDecision,
+    GateEvaluation,
+    GateKind,
+    GateOutcome,
+    admit_p1_05_ready_execution,
+    build_p1_05_gate_decision,
+    evaluate_p1_05_gates,
+)
 from .identity import Identity
 from .security import ActorContext, OrganizationScope, Principal
 from .workflow import (
@@ -26,6 +35,10 @@ __all__ = [
     "CanonicalRecord",
     "ExecutionContext",
     "ExecutionLifecycle",
+    "GateDecision",
+    "GateEvaluation",
+    "GateKind",
+    "GateOutcome",
     "GovernedVersionPin",
     "Identity",
     "OperationSideEffectClass",
@@ -34,7 +47,10 @@ __all__ = [
     "WorkflowDefinition",
     "WorkflowLifecycle",
     "WorkflowOperation",
+    "admit_p1_05_ready_execution",
     "build_p1_02_native_record",
     "build_p1_03_workflow",
+    "build_p1_05_gate_decision",
+    "evaluate_p1_05_gates",
     "start_p1_04_execution",
 ]
